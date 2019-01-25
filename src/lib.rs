adc632f2105674d72839f97b9debc91c30720ce7
Pete Gadomski <pete.gadomski@gmail.com>
2019-01-25 09:36:17 -0700

    Add first bit of sanity tests

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,17 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_snake_case)]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::ptr;
+
+    #[test]
+    fn gdal_data_path() {
+        unsafe {
+            let size = PDALGetGdalDataPath(ptr::null_mut(), 1024);
+            assert_eq!(0, size);
+        }
+    }
+}
